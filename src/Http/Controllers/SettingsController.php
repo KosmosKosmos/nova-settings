4ad82dac9344297d7ecdf566d6de317f1c3a44db
Vladimir Bubar <vladimir.bubar@kosmoskosmos.de>
2020-02-07 14:20:52 +0100

    [ADD] Encrypt and decrypt fields.

--- src/Http/Controllers/SettingsController.php
+++ src/Http/Controllers/SettingsController.php
@@ -2,10 +2,12 @@
 
 namespace Epigra\NovaSettings\Http\Controllers;
 
+use Illuminate\Contracts\Encryption\DecryptException;
 use Illuminate\Routing\Controller;
 use Illuminate\Http\Request;
 use Epigra\NovaSettings\NovaSettingsTool;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\Log;
 use Laravel\Nova\Contracts\Resolvable;
 use Laravel\Nova\Http\Requests\NovaRequest;
@@ -18,7 +20,17 @@ class SettingsController extends Controller
 
         $fields->whereInstanceOf(Resolvable::class)->each(function (&$field) {
             if (!empty($field->attribute)) {
-                $field->resolve([$field->attribute => setting($field->attribute)]);
+                $key = $field->attribute;
+                $value = setting($key);
+                if (strpos($key, 'encrypted_') === 0) {
+                    $key = substr_replace($key, '', 0, 10);
+                    try {
+                        $value = Crypt::decrypt(setting($key));
+                    } catch (DecryptException $e) {
+                        $value = setting($key);
+                    }
+                }
+                $field->resolve([$field->attribute => $value]);
             }
         });
 
@@ -36,7 +48,18 @@ class SettingsController extends Controller
             $field->fill($request, $tempResource);
 
             if (property_exists($tempResource, $field->attribute)) {
-                setting([$field->attribute => $tempResource->{$field->attribute} ]);
+                $key = $field->attribute;
+                $value = $tempResource->{$field->attribute};
+
+                if (strpos($key, 'encrypted_') === 0) {
+                    $key = substr_replace($key, '', 0, 10);
+                    try {
+                        Crypt::decrypt($value);
+                    } catch (DecryptException $e) {
+                        $value = Crypt::encrypt($value);
+                    }
+                }
+                setting([$key =>  $value]);
             }
 
         });
